c64a18a4b7f96e7728808d42498d5015bd35271b
한예주 <han-yejoo@han-yejuui-MacBook-Pro.local>
2020-08-17 14:44:44 +0900

    make board (without number of mines)

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -2,16 +2,30 @@
 #include <vector>
 using namespace std;
 
-void printboard (vector <vector <int> > &board);
+vector <vector <bool> > makeboard(int width, int height);
+void printboard (vector <vector <bool> > &board);
 
 int main (int argc, char** argv)
 {
-  vector <vector <int> > board{ {2, 3},
-                                {3, 1} };
+  srand(time(0));
+  vector <vector <bool> > board = makeboard(5, 5);
   printboard(board);
 }
 
-void printboard (vector <vector <int> > &board)
+vector <vector <bool> > makeboard(int width, int height)
+{
+  vector <vector <bool> > board(width, vector<bool>(height));
+  for (int i = 0; i < width; i++)
+  {
+    for (int j = 0; j < height; j++)
+    {
+      board[i][j] = rand() % 2;
+    }
+  }
+  return board;
+}
+
+void printboard (vector <vector <bool> > &board)
 {
   for (int i = 0; i < board.size(); i++)
   {
