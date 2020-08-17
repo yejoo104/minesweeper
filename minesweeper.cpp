4fd9d860b8e610acb92a4737b2ad68e20a5ba5e0
한예주 <han-yejoo@han-yejuui-MacBook-Pro.local>
2020-08-17 16:07:01 +0900

    modified print board

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 vector <vector <bool> > makeboard(int width, int height, int mines);
-void printboard (vector <vector <bool> > &board);
+void printboard (vector <vector <int> > &board);
 
 int main (int argc, char** argv)
 {
-  vector <vector <bool> > board = makeboard(4, 5, 3);
+  vector <vector <int> > board = { {-1, -1, -1}, {0, 4, 3}, {2, 3, 2} };
   printboard(board);
 }
 
@@ -37,14 +37,25 @@ vector <vector <bool> > makeboard(int width, int height, int mines)
   return board;
 }
 
-void printboard (vector <vector <bool> > &board)
+void printboard (vector <vector <int> > &board)
 {
   for (int i = 0; i < board.size(); i++)
   {
     for (int j = 0; j < board[i].size(); j++)
     {
-      cout << board[i][j] << " ";
+      if (board[i][j] == -1)
+      {
+        cout << "? ";
+      }
+      else if (board[i][j] == 0)
+      {
+        cout << "_ ";
+      }
+      else
+      {
+        cout << board[i][j] << " ";
+      }
     }
-    cout << "\n";
+    cout << endl;
   }
 }
