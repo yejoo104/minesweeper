fe2c85efaa6c624be77b26962fcc035d4e079699
한예주 <han-yejoo@han-yejuui-MacBook-Pro.local>
2020-08-17 23:19:03 +0900

    initialize level

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <string>
 using namespace std;
 
 vector <vector <bool> > makeboard(int width, int height, int mines);
@@ -8,8 +9,48 @@ void printboard (vector <vector <int> > &board);
 
 int main (int argc, char** argv)
 {
-  vector <vector <int> > board = { {-1, -1, -1}, {0, 4, 3}, {2, 3, 2} };
-  printboard(board);
+  string level;
+  int width;
+  int height;
+  int mines;
+
+  while (true)
+  {
+    cout << "Type in B for Beginner (10x10 with 10 mines), I for Intermediate(16x16 with 40 mines), E for Expert(30x16 with 99 mines), and C for Custom Size." << endl;
+    cin >> level;
+    if (level == "B")
+    {
+      width = 10;
+      height = 10;
+      mines = 10;
+      break;
+    }
+    if (level == "I")
+    {
+      width = 16;
+      height = 16;
+      mines = 40;
+      break;
+    }
+    if (level == "E")
+    {
+      width = 30;
+      height = 16;
+      mines = 99;
+      break;
+    }
+    if (level == "C")
+    {
+      cout << "Type in custom width." << endl;
+      cin >> width;
+      cout << "Type in custom height." << endl;
+      cin >> height;
+      cout << "Type in custom number of mines." << endl;
+      cin >> mines;
+      break;
+    }
+  }
+
 }
 
 vector <vector <bool> > makeboard(int width, int height, int mines)
