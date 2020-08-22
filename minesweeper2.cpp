0f269c48e6bd0ade946946f6c4afdeb6583d040d
한예주 <yejoo_han@college.harvard.edu>
2020-08-22 22:26:50 +0900

    first click

--- minesweeper2.cpp
+++ minesweeper2.cpp
@@ -2,8 +2,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <random>
+#include <queue>
 using namespace sf;
 using namespace std;
+
+vector <vector <bool> > makeboard(int width, int height, int mines, int row, int column);
+vector <vector <int> > updatetracker(vector < vector <int> > tracker, vector <vector <bool> > mineboard, int row, int column);
+int around(vector <vector <bool> > mineboard, int row, int column);
+void printarray (vector <vector <bool> > board);
+void printarray (vector <vector <int> > board);
 
 int main()
 {
@@ -20,13 +28,28 @@ int main()
   RenderWindow app(VideoMode(w * width, w * height), "Minesweeper");
 
   // Display Board before Game Starts
+  int firstrow;
+  int firstcol;
   while (app.isOpen())
   {
+    Vector2i pos = Mouse::getPosition(app);
+    int x = pos.x / w;
+    int y = pos.y / w;
+
     Event e;
     while (app.pollEvent(e))
     {
       if (e.type == Event::Closed) app.close();
     }
+
+    // First Click
+    if (e.type == Event::MouseButtonPressed)
+      if (e.key.code == Mouse::Left)
+        {
+          firstcol = x;
+          firstrow = y;
+          break;
+        }
 
     for (int i = 0; i <= height;i++)
       for (int j = 0; j <= width; j++)
@@ -39,5 +62,142 @@ int main()
     app.display();
   }
 
-  // First Click
+  // Initialize Board & Tracker after First Click
+  cout << firstrow << firstcol << endl;
+  vector <vector <int> > tracker(height, vector<int>(width, -1));
+  vector <vector <bool> > mineboard = makeboard(width, height, mines, firstrow, firstcol);
+  tracker = updatetracker(tracker, mineboard, firstrow, firstcol);
+
+  printarray(tracker);
+  printarray(mineboard);
+
+  while (app.isOpen())
+  {
+    Vector2i pos = Mouse::getPosition(app);
+    int x = pos.x / w;
+    int y = pos.y / w;
+
+    Event e;
+    while (app.pollEvent(e))
+    {
+      if (e.type == Event::Closed) app.close();
+    }
+
+    app.clear(Color::White);
+    for (int i = 0; i < tracker.size(); i++)
+      for (int j = 0; j < tracker[i].size(); j++)
+      {
+        if (tracker[i][j] == -1) s.setTextureRect(IntRect(10 * w, 0, w, w));
+        else s.setTextureRect(IntRect(tracker[i][j] * w, 0, w, w));
+        s.setPosition (j * w, i * w);
+        app.draw(s);
+      }
+
+    app.display();
+  }
+}
+
+vector <vector <bool> > makeboard(int width, int height, int mines, int row, int column)
+{
+  random_device device;
+  mt19937 generator(device());
+  uniform_int_distribution<> widthdis(0, width - 1);
+  uniform_int_distribution<> heightdis(0, height - 1);
+
+  int count = 0;
+  float remaining = width * height;
+
+  vector <vector <bool> > board(height, vector<bool>(width));
+
+  while (count < mines)
+  {
+    int thisrow = heightdis(generator);
+    int thiscolumn = widthdis(generator);
+
+    if (thisrow == row && thiscolumn == column) continue;
+
+    if (!board[thisrow][thiscolumn])
+    {
+      board[thisrow][thiscolumn] = true;
+      count++;
+    }
+  }
+
+  return board;
+}
+
+vector <vector <int> > updatetracker(vector < vector <int> > tracker, vector <vector <bool> > mineboard, int row, int column)
+{
+  queue <vector <int> > myqueue;
+  myqueue.push({row, column});
+  while (!myqueue.empty())
+  {
+    vector <int> coordinates = myqueue.front();
+    myqueue.pop();
+    int minesnearby = around(mineboard, coordinates[0], coordinates[1]);
+    tracker[coordinates[0]][coordinates[1]] = minesnearby;
+
+    if (minesnearby == 0)
+    {
+      for (int i = -1; i <= 1; i++)
+      {
+        if(coordinates[0] + i >= 0 && coordinates[0] + i < mineboard.size())
+        {
+          for (int j = -1; j <= 1; j++)
+          {
+            if(coordinates[1] + j >= 0 && coordinates[1] + j < mineboard[0].size() && tracker[coordinates[0] + i][coordinates[1] + j] == -1)
+            {
+              myqueue.push({coordinates[0] + i, coordinates[1] + j});
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return tracker;
+}
+
+int around(vector <vector <bool> > mineboard, int row, int column)
+{
+  int count = 0;
+  for (int i = -1; i <= 1; i++)
+  {
+    if (row + i >= 0 && row + i < mineboard.size())
+    {
+      for (int j = -1; j <= 1; j++)
+      {
+        if (column + j >= 0 && column + j < mineboard[0].size())
+        {
+          if (mineboard[row + i][column + j]) count++;
+        }
+      }
+    }
+  }
+
+  return count - mineboard[row][column];
+}
+
+void printarray (vector <vector <bool> > board)
+{
+  for (int i = 0; i < board.size(); i++)
+  {
+    for(int j = 0; j < board[i].size(); j++)
+    {
+      cout << board[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+void printarray (vector <vector <int> > board)
+{
+  for (int i = 0; i < board.size(); i++)
+  {
+    for(int j = 0; j < board[i].size(); j++)
+    {
+      cout << board[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
